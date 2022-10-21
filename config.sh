070a1f492dccef5b27b92dbfdc0bcb5e4052f77e
cvk1988 <59971044+cvk1988@users.noreply.github.com>
2022-10-21 12:40:27 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,26 +3,26 @@ export CWD=$PWD
 ###### #Parameters 
 
 # Location of raw data 
-export RAW="/xdisk/uschuch/corykeith/sim_mock_comm/viruses/begomovirus/sim_resist/mock_reads"
-export OUT_DIR="/xdisk/uschuch/corykeith/mock_ViCC/out"
-export PROFILE="/xdisk/uschuch/corykeith/mock_ViCC/profile.txt"
+export RAW="/CWD/test_data/sim_resist/mock_reads"
+export OUT_DIR="/CWD/out"
+export PROFILE="/CWD/profile.txt"
 
 # Location of tools and indices
-export BOWTIE="/home/u32/corykeith/tools/bowtie2-2.4.1-linux-x86_64"
-export SPADES="/home/u32/corykeith/tools/SPAdes-3.14.1-Linux/bin"
-export SEQKIT="/xdisk/uschuch/corykeith/tools/seqkit"
-export CSVTK="/xdisk/uschuch/corykeith/tools/csvtk"
+export BOWTIE="CWD/tools/bowtie2-2.4.1-linux-x86_64"
+export SPADES="CWD/tools/SPAdes-3.15.5-Linux/bin"
+export SEQKIT="CWD/tools/seqkit"
+export CSVTK="CWD/tools/csvtk"
 export BLASTDB="CWD/blastdb/[database]"
-export BLAST1line="/xdisk/uschuch/corykeith/BLAST/BLdb_allsats/BLdb_CLCuD_helper_sats_1line.fa"
-export BAMTOOLS="/xdisk/uschuch/corykeith/tools/bin"
+export BLAST1line="CWD/blastdb/[database]_1line.fa"
+export BAMTOOLS="CWD/tools/bin"
 
 #Place to store scripts
 export SCRIPT_DIR="$PWD/scripts"
 export GATHER="$PWD/scripts"
 # User information
 export QUEUE="standard"
-export GROUP="uschuch"
-export MAIL_USER="corykeith@email.arizona.edu"
+export GROUP="[group_name]"
+export MAIL_USER="[user_email@email.com]"
 export MAIL_TYPE="END"
 
 
